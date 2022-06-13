b3aee999bb70495a04c440ed5622d4f437537dd4
NikolNikolaeva <92273054+NikolNikolaeva@users.noreply.github.com>
2022-06-13 21:45:14 +0300

    Update Scrabble.cpp

--- Scrabble.cpp
+++ Scrabble.cpp
@@ -5,11 +5,6 @@
 * Introduction to programming course
 * Faculty of Mathematics and Informatics of Sofia University
 * Winter semester 2021/2022
-*
-* @author Nikol Nikolaeva
-* @idnumber 2MI0600079
-* @compiler VC
-*
 * cpp file with the functions and main function
 *
 */
